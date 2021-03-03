973d029a8b170d0e2084a23360b0c4c31be1feaf
Maxime Debreuil <maxime.debreuil@gmail.com>
2021-03-03 19:47:27 +0100

    a
    a

--- view/boxes/accueil.php
+++ view/boxes/accueil.php
@@ -1,2 +1,40 @@
 
 
+<body>
+    <figure>
+        <img src="https://media-cdn.tripadvisor.com/media/photo-s/12/5c/0b/8e/getlstd-property-photo.jpg" alt="banniere" id="banniere"/>
+    </figure>
+    <br>
+    <button>Faites vous livrer</button>
+    <br><br>
+    <button>Prenez à emporter</button>
+    <br>
+    <span>Bienvenue à La Fourchette à Bretelle, le restautant où la vie est plus belle</span>
+    <br><br><br>
+    <div>
+        <div>
+            <img src="https://icones8.fr/vue-static/icon/svg/paintPalette.svg" alt="" id=""/>
+            <span>Un intérieur sobre et élégant</span>
+        </div>
+        <div>
+            <img src="https://icones8.fr/vue-static/icon/svg/paintPalette.svg" alt="" id=""/>
+            <span>Une carte alléchante</span>
+        </div>
+        <div>
+            <img src="https://icones8.fr/vue-static/icon/svg/paintPalette.svg" alt="" id=""/>
+            <span>Une équipe chaleureuse</span>
+        </div>
+        <div>
+            <img src="https://icones8.fr/vue-static/icon/svg/paintPalette.svg" alt="" id=""/>
+            <span>Zéro gaspis à la carte</span>
+        </div>
+    </div>
+    <br><br>
+    <span>Quelques unes de nos spécialités</span>
+    <br>
+    <img src="https://static.emilehenry.com/866768-large_default/plat-a-four-ovale.jpg" alt="" id=""/>
+    <img src="https://static.emilehenry.com/866768-large_default/plat-a-four-ovale.jpg" alt="" id=""/>
+    <img src="https://static.emilehenry.com/866768-large_default/plat-a-four-ovale.jpg" alt="" id=""/>
+</body>
+
+
