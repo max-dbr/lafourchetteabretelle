e3427cfbf4d6b4e30ff9e429ef2aadeaf6208d56
Maxime Debreuil <maxime.debreuil@gmail.com>
2021-03-04 14:16:17 +0100

    aaaa
    aaaa

--- view/boxes/accueil.php
+++ view/boxes/accueil.php
@@ -44,7 +44,9 @@
      <div class="carousel-item">
      <div class="carouselBloc"><img src="http://assets.suelo.pl/soup/img/photos/special-pizza.jpg" class="d-block w-50" alt="..." class="carrousel">
       <div class="carrouselDiv">
-        Helloooooooooooooo World</div>
+      <span class="titreMenuCarousel">L'Abdel Burger</span> <br><br>
+      <span>Un pain moelleux avec éclats de maïs, une sauce goût fumé, des oignons croustillants, des onion rings, des tranches de bacon , des tranches de cheddar fondu et une viande de bœuf grillée à la flamme !</span>
+    </div>
     </div>
     </div>
     <div class="carousel-item">
