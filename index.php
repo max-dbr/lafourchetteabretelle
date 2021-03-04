1a73e0fddf723e2717197eabd1ef8924114bf48a
Maxime Debreuil <maxime.debreuil@gmail.com>
2021-03-04 01:15:08 +0100

    maj max
    maj max

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
         <link rel="icon" type="image/png" href="./asset/images/favicon.png" />
         <meta name="theme-color" content="#e87e04">
         <link href="https://fonts.googleapis.com/css?family=Montserrat&amp;display=swap" rel="stylesheet">
+        <link href="http://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <script src="http://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 
     </head>
     <body>
--- view/boxes/accueil.php
+++ view/boxes/accueil.php
@@ -1,40 +1,59 @@
-
-
 <body>
-    <figure>
-        <img src="https://media-cdn.tripadvisor.com/media/photo-s/12/5c/0b/8e/getlstd-property-photo.jpg" alt="banniere" id="banniere"/>
-    </figure>
+    <figure><img src="https://media-cdn.tripadvisor.com/media/photo-s/12/5c/0b/8e/getlstd-property-photo.jpg" alt="banniere" class="banniere"/></figure>
     <br>
-    <button>Faites vous livrer</button>
+    <a href="./index.php"><button id ="callToAction1">Faites vous livrer</button></a>
     <br><br>
-    <button>Prenez à emporter</button>
+    <a href="./index.php"><button id ="callToAction2">Prenez à emporter</button></a>
+    <div id="bienvenueAccueil">Bienvenue à La Fourchette à Bretelle, le restautant où la vie est plus belle</div>
     <br>
-    <span>Bienvenue à La Fourchette à Bretelle, le restautant où la vie est plus belle</span>
-    <br><br><br>
-    <div>
-        <div>
-            <img src="https://icones8.fr/vue-static/icon/svg/paintPalette.svg" alt="" id=""/>
-            <span>Un intérieur sobre et élégant</span>
+    <div id=iconeBienvenue>
+
+
+        <div class="hover14 column">
+            <figure><img src="https://icones8.fr/vue-static/icon/svg/paintPalette.svg" alt="" id=""/></figure>
+            <div class="iconeSpan">Un intérieur sobre et élégant</div>
         </div>
-        <div>
-            <img src="https://icones8.fr/vue-static/icon/svg/paintPalette.svg" alt="" id=""/>
-            <span>Une carte alléchante</span>
+
+        <div class="hover14 column">
+            <figure><img src="https://icones8.fr/vue-static/icon/svg/paintPalette.svg" alt="" id=""/></figure>
+            <div class="iconeSpan">Une carte alléchante</div>
         </div>
-        <div>
-            <img src="https://icones8.fr/vue-static/icon/svg/paintPalette.svg" alt="" id=""/>
-            <span>Une équipe chaleureuse</span>
+
+        <div class="hover14 column">
+            <figure><img src="https://icones8.fr/vue-static/icon/svg/paintPalette.svg" alt="" id=""/></figure>
+            <div class="iconeSpan">Une équipe chaleureuse</div>
         </div>
-        <div>
-            <img src="https://icones8.fr/vue-static/icon/svg/paintPalette.svg" alt="" id=""/>
-            <span>Zéro gaspis à la carte</span>
+        <div class="hover14 column">
+            <figure><img src="https://icones8.fr/vue-static/icon/svg/paintPalette.svg" alt="" id=""/></figure>
+            <div class="iconeSpan">Zéro gaspis à la carte</div>
         </div>
     </div>
-    <br><br>
-    <span>Quelques unes de nos spécialités</span>
     <br>
-    <img src="https://static.emilehenry.com/866768-large_default/plat-a-four-ovale.jpg" alt="" id=""/>
-    <img src="https://static.emilehenry.com/866768-large_default/plat-a-four-ovale.jpg" alt="" id=""/>
-    <img src="https://static.emilehenry.com/866768-large_default/plat-a-four-ovale.jpg" alt="" id=""/>
+    <div id="specialites">Quelques unes de nos spécialités</div>
+    <br>
+
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://static.emilehenry.com/866768-large_default/plat-a-four-ovale.jpg" class="d-block w-100" alt="..." class="carrousel">
+    </div>
+    <div class="carousel-item">
+      <img src="https://static.emilehenry.com/866768-large_default/plat-a-four-ovale.jpg" class="d-block w-100" alt="..." class="carrousel">
+    </div>
+    <div class="carousel-item">
+      <img src="https://static.emilehenry.com/866768-large_default/plat-a-four-ovale.jpg" class="d-block w-100" alt="..." class="carrousel">
+    </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+    </button>
+    </div>
+
 </body>
 
 
