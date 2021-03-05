46ea77dd7d35dfd33b7b1c2f9b967999cba2a7e5
Thomas DELMAS <thomas.julien.delmas@gmail.com>
2021-03-05 02:12:44 +0100

    Thomas DELMAS
    modif css page compte

--- view/boxes/gestion.php
+++ view/boxes/gestion.php
@@ -1,3 +1,16 @@
+<div id="compte">
+    <h2>Mon Compte</h2>
+    <br>
+    <span>Bienvenue <?php echo $_SESSION['PRENOM'], " " , $_SESSION['NOM'] ?></span>
+    <br>
+    <span>Vous avez <?php echo $_SESSION['FIDELITE'] ?> points</span>
+    <br>
+    <div class="progress">
+        <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+    </div>
+    <span>Tout les 1000 points, recevez une remise de 5€ sur votre prochaine commande</span>
+</div>
+
 <form method ="POST" action="./index.php?page=profil">
 <input type="submit" name="Modifier" value="MODIFIER MES INFOS" >
 </form>
