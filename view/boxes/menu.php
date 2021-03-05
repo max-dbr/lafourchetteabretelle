fef1d3789ecbba19026678fded07f1f89d2f87a1
Abdelfattah <Abdelfattah.karam@gmail.com>
2021-03-05 03:16:48 +0100

    menu

--- view/boxes/menu.php
+++ view/boxes/menu.php
@@ -37,3 +37,20 @@
         </ul>
     </nav>
 </header>
+<nav class="menu2">
+        <ul>
+            <li><a href="./index.php" ><img class="logo" src="./asset/images/la-fourchette-a-bretelles-logoV2-02.png"></a></li>
+        </ul>
+        <img src="/lafourchetteabretelle/asset/images/Hamburger_icon.svg.png" id="humbergerImage" onclick="afficher()"/>
+        <div id="menuHumberger">
+        <br>
+            <div><a href="./index.php" class="overeffect">Accueil</a><div>
+            <br>
+            <div><a href="./index.php?page=carte" class="overeffect">Carte</a></div>
+            <br>
+            <div><a href="./index.php?page=gestion" class="overeffect">Mon Compte</a></div>
+            <br>
+            <div><a href="./execution/deconnexion.php" class="overeffect">Déconnexion</a></div>
+            <br>
+            <img src="/lafourchetteabretelle/asset/images/close.png" id="close" onclick="masquer()"/>
+        </div>
